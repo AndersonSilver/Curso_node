bf0b2caba30050d78a0ad0ae24c2c85fcad2409d
AndersonSilver <andersonsilver18@gmail.com>
2023-03-17 00:43:18 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -4,16 +4,35 @@ const app = express(); // Iniciando o Express
 
 app.get("/",function(req,res){
 
-    res.send("Bem vindo ao guia do programador");
+    res.send("<h1>Bem vindo ao guia do programador</h1>");
+    // SEND => Função que envia dado para o usuario
+});
+
+app.get("/blog/:artigo?", function(req,res){
+
+    let artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("Artigo" + artigo + "")
+    }else{
+        res.send("Bem vindo ao meu blog");
+    }
 
 });
-app.get("/blog", function(req,res){
 
-    res.send("Bem vind ao meu blog");
-
-});
 app.get("/youtube", function(req,res){
     res.send("Ola, seja bem vindo ao meu canal do youtube");
+});
+app.get("/majo/:nome/:empresa", function(req,res){
+    //req => Dados enviado pelo usuario
+    //res => Resposta enviada para o usuario
+
+    // res.send("Ola, seja bem vindo ao meu canal do youtube");
+
+    let empresa = req.params.empresa
+    let nome = req.params.nome 
+    res.send("Ola " + nome + " tudo bem? sou da empresa " + empresa );
+
 });
 
 
